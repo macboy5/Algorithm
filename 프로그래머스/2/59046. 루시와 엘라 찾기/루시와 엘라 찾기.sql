87b0a7707bbf2f37e5a2ebecdb5426a0d4b72660
박성진 <106302098+macboy5@users.noreply.github.com>
2026-04-08 16:25:10 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,	NAME,	SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME = 'Lucy' OR NAME = 'Ella' OR NAME = 'Pickle' or NAME = 'Rogan' OR
+    NAME = 'Sabrina' OR NAME = 'Mitty'
+;
