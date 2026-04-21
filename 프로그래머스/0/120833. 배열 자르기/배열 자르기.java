c1f159a953e6caf06b2e600cbb5f9592fd34dda3
박성진 <106302098+macboy5@users.noreply.github.com>
2026-04-21 13:01:40 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int length = num2-num1+1;
+        
+        
+        int[] answer = new int[length];
+        
+        for(int i=num1; i<=num2; i++){
+            answer[i-num1] = numbers[i];
+        }
+        
+        return answer;
+    }
+}
