2353d35e31d511d543a676d513a9c53cb71fa13c
박성진 <106302098+macboy5@users.noreply.github.com>
2026-06-29 15:38:16 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        if(M==1 && N==1) return 0;
+        
+        
+        answer = M*N-1;
+
+        return answer;
+    }
+}
