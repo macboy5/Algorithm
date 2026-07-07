82bf98e4788eefdfec04de150f4540781b145261
박성진 <106302098+macboy5@users.noreply.github.com>
2026-07-07 21:36:14 +0900

    [D2] Title: 두 개의 숫자열, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
@@ -1,73 +1,61 @@
-#include<iostream>
-#include<vector>
+import java.util.*;
+import java.io.FileInputStream;
 
-using namespace std;
 
-int calculate(vector<int>&A, vector<int>&B, int N , int M){
-    int diff;
-    int max = -1;
-    if(N>M) {
-        diff = N - M;
-
-        for(int j=0; j<=diff ; j++){
-            int sum = 0;
-            for(int i=0; i<M; i++){
-                sum += A[i+j] * B[i];
-            }
-            if(max < sum) max = sum;
-        }
-    }
-    else {
-        diff = M - N;
-        for(int j=0; j<=diff ; j++){
-            int sum = 0;
-            for(int i=0; i<N; i++){
-                sum += A[i] * B[i+j];
-            }
-            if(max < sum) max = sum;
-        }
-
-    }
-
-    return max;
-
-}
-
-int main(int argc, char** argv)
+class Solution
 {
-    int test_case;
-    int T;
-    /*
-       아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
-       앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-       //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-       freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-       따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
-       freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
-       단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
-    */
-    //freopen("input.txt", "r", stdin);
-    cin>>T;
-    /*
-       여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-    */
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
+	public static void main(String args[]) throws Exception
+	{
 
-        int N, M;
-        cin >> N >> M;
-        vector<int> A(N);
-        vector<int> B(M);
-        for(int i=0; i<N; i++){
-            cin >> A[i];
-        }
-        for(int i=0;i<M; i++){
-            cin >> B[i];
-        }
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
 
-        cout <<"#" << test_case <<" "<< calculate(A,B,N,M) << "\n";
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N, M;
+            N = sc.nextInt();
+            M = sc.nextInt();
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            for(int i=0; i<N; i++){
+            	arr1[i] = sc.nextInt();
+            }
+            for(int i=0; i<M; i++){
+            	arr2[i] = sc.nextInt();
+            }
+                  
+            int answer=-1;
+            
+            
+            if(N<=M){
+                
+                for(int start=0; start<=M-N; start++){
+                    int sum = 0;
+                    for(int i=0; i<N; i++){
+                        sum += arr1[i] * arr2[start+i];
+                    }
+                    answer = Math.max(answer, sum);
+                }
+            
+            }
+            else{
+                
+                    for(int start=0; start<=N-M; start++){
+                    int sum = 0;
+                    for(int i=0; i<M; i++){
+                        sum += arr1[start+i] * arr2[i];
+                    }
+                    answer = Math.max(answer, sum);
+                }
+            
+            }
+            
 
-
-    }
-    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+            System.out.println("#" + test_case + " " + answer);
+            
+		}
+	}
 }
