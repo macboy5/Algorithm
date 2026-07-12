3ee30b2c57f06970ad0e848b43e85f4d3fac369b
박성진 <106302098+macboy5@users.noreply.github.com>
2026-07-12 14:07:24 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(char c : my_string.toCharArray()){
+            if(c >= 'A' && c <= 'Z'){
+                answer[c-65]++;
+            }
+            else{
+                answer[c-97+26]++;
+            }
+        }
+        
+        return answer;
+    }
+}
