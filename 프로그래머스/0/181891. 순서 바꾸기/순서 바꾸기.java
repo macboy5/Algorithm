55c3ee39086e3e58e3c431e53b476047fb6d98fa
박성진 <106302098+macboy5@users.noreply.github.com>
2026-07-14 08:46:48 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.20 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=n; i<num_list.length; i++){
+            list.add(num_list[i]);
+        }
+        for(int i=0; i<n ; i++){
+            list.add(num_list[i]);
+        }
+        
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
