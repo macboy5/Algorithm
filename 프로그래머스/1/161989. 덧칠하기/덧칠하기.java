56dadbb3e6ce5d5e21fa9bda5b2b598945d9842b
박성진 <106302098+macboy5@users.noreply.github.com>
2026-07-27 11:57:29 +0900

    [level 1] Title: 덧칠하기, Time: 1.29 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        int pos = -1;
+        
+        for(int i=0; i<section.length; i++){
+            if(section[i] <= pos){
+                    continue;
+            }
+            
+            pos = section[i] + m - 1;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
